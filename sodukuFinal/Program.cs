6398b48ee32d9fa8e4757d27bb16de0e7319eb2a
tal lubezky hess <talilo12345@gmail.com>
2022-01-20 14:39:48 +0200

    [[[

---
+++ sodukuFinal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sodukuFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
